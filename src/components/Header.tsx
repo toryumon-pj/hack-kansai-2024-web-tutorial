be46e513f89352be716695dcfb1d19692d0b0f5d
Yuma Satake <satous.y@icloud.com>
2024-06-01 22:16:00 +0900

    feat: ホームに戻るボタン実装

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
-import { Stack, Typography } from '@mui/material';
+import { Button, Stack, Tooltip, Typography } from '@mui/material';
 import { FC } from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import { useNavigate } from 'react-router-dom';
 
 type Props = {
   title: string;
@@ -9,13 +11,31 @@ type Props = {
  * 画面上部のヘッダー
  */
 export const Header: FC<Props> = ({ title }) => {
+  const router = useNavigate();
+
+  /**
+   * 戻るボタンのハンドリング
+   */
+  const handleClickHomeButton = (): void => {
+    router('/');
+  };
+
   return (
     <Stack
+      direction="row"
+      justifyContent="flex-start"
+      alignItems="center"
+      spacing={2}
       sx={{
         height: '100%',
-        justifyContent: 'center',
+        width: '100%',
       }}
     >
+      <Tooltip title="ホーム">
+        <Button onClick={handleClickHomeButton} variant="outlined">
+          <HomeIcon />
+        </Button>
+      </Tooltip>
       <Typography variant="h6">{title}</Typography>
     </Stack>
   );
