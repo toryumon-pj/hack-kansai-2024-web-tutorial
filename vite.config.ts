064cad56ffbb46efdaacfe49c8e8ce72619af722
Yuma Satake <satous.y@icloud.com>
2024-06-01 23:31:04 +0900

    feat: エイリアス設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 import writeBuiltTimePlugin from './plugins/writeBuiltTimePlugin';
 
 const buildDir = 'dist';
@@ -11,6 +12,13 @@ export default defineConfig({
   server: {
     strictPort: true,
     port: 3000,
+  },
+  base: './',
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   plugins: [react(), writeBuiltTimePlugin(buildDir)],
 });
